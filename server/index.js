29527b6029fd7cf03f0ec48f5ee38da95ae7ed14
mekrishna26s <murari2632000@gmail.com>
2025-05-02 11:09:05 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import authRoutes from './routes/authRoutes.js';
+import taskRoutes from './routes/taskRoutes.js';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => console.log('Server started on port', process.env.PORT));
+  })
+  .catch(err => console.error(err));
---
+++ server/models/task.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  dueDate: Date,
+  priority: String,
+  status: String,
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', taskSchema);
