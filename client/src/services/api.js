7e7f3844bb83e1173ed085462f6b3739c3471a7d
mekrishna26s <murari2632000@gmail.com>
2025-05-07 08:46:06 +0530

    making correction in api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,8 @@
+import axios from 'axios';
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
+  withCredentials: true
 });
+
+export default API;
