0e03787bd41ca4229cb100c807f472b546a02d9e
mekrishna26s <murari2632000@gmail.com>
2025-05-08 00:56:14 +0530

    made changes in api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,15 @@
+// import axios from 'axios';
+
+// const API = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+//   withCredentials: true,
+// });
+
+// export default API;
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
